50b03a7f7c1aaa9abf27db186c360cde67de8a3b
Jothaka <jhkahle@hotmail.de>
2021-01-23 15:55:59 +0100

    Added: basic playercontroller, targetIndicator
     player receives inputcomponent based on platform, targetIndicator is drawn when user touches the screen does not allow target to be under or behind the player.

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -1,17 +1,45 @@
 ﻿using UnityEngine;
+using System;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField]
     private CameraController cameraController;
 
+    [SerializeField]
+    private Transform targetIndicatorOrigin;
+
+    [SerializeField]
+    private LineRenderer targetIndicator;
+
+    private PlayerController player;
+
+
     void Start()
     {
         cameraController.StartCameraPan();
+        cameraController.CameraMovedToPlayer += CreatePlayer;
     }
 
     void Update()
     {
-        
+        if (player != null)
+            player.Update();
+    }
+
+    private void CreatePlayer()
+    {
+        switch (Application.platform)
+        {
+            case RuntimePlatform.WindowsPlayer:
+            case RuntimePlatform.WindowsEditor:
+                player = new PlayerController(targetIndicatorOrigin, new MouseInputComponent(), targetIndicator);
+                break;
+            case RuntimePlatform.Android:
+                player = new PlayerController(targetIndicatorOrigin, new TouchInputComponent(), targetIndicator);
+                break;
+            default:
+                throw new NotImplementedException(Application.platform.ToString());
+        }
     }
 }
---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController
+{
+    private IInputComponent input;
+    private Transform targetIndicatorOrigin;
+    private LineRenderer targetIndicator;
+
+    public PlayerController(Transform targetOrigin, IInputComponent inputComponent, LineRenderer targetIndicator)
+    {
+        targetIndicatorOrigin = targetOrigin;
+        input = inputComponent;
+        this.targetIndicator = targetIndicator;
+        targetIndicator.enabled = false;
+    }
+
+    public void Update()
+    {
+        if(input.IsInputPressed())
+        {
+            Vector3 inputPosition = input.GetInputPosition();
+            var directionVector = (inputPosition - targetIndicatorOrigin.position).normalized;
+            if(directionVector.y >= 0 && directionVector.x >= 0)
+            {
+                var linePositions = new Vector3[2] { targetIndicatorOrigin.position, inputPosition };
+                targetIndicator.enabled = true;
+                targetIndicator.positionCount = 2;
+                targetIndicator.SetPositions(linePositions);
+            }
+        }
+    }
+}
